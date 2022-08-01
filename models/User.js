9381ddb5883f0b35e4811a2e0d17a49a0f940dc4
Indo-Luciano <99900188+Indo-Luciano@users.noreply.github.com>
2022-08-01 12:53:25 -0500

    Add models folder

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { Sequelize, DataTypes, DATE } = require("sequelize");
+const sequelize = new Sequelize("sqlite::memory:");
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    firstname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    date: {
+      type: DataTypes.DATE,
+      default: Date.now,
+    },
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  });
+  return User;
+};
